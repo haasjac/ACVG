4873da01c4d09f29672625a8b239a02dfbbc1944
Andrew Delamar <andrewdelamar@andrewmacbookair.local>
2016-11-13 22:03:03 -0500

    more navigator changes

--- Assets/_Scripts/buttonNavigation.cs
+++ Assets/_Scripts/buttonNavigation.cs
@@ -2,8 +2,6 @@
 using UnityEngine.SceneManagement;
 
 public class buttonNavigation : MonoBehaviour {
-    public GameObject loginAlert;
-
 	// Use this for initialization
 	void Start () {
 	}
@@ -22,44 +20,10 @@ public class buttonNavigation : MonoBehaviour {
     }
 
     public void touchProfileButton() {
-        SceneManager.LoadScene("FB_test");
-        /*
-        // check logged in user (-1 == not logged in)
-        if (PlayerPrefs.HasKey("loggedInUser")) {
-            if (PlayerPrefs.GetInt("loggedInUser") != -1) {
-                // logged in, show profile page
-                SceneManager.LoadScene("mainProfile");
-            }
-            else {
-                // not logged in, show alert
-                loginAlert.SetActive(true);
-            }
-        }
-        else {
-            // never logged in before, create key, show alert
-            PlayerPrefs.SetInt("loggedInUser", -1);
-            loginAlert.SetActive(true);
-        }
-        */
-    }
-
-    public void touchSettingsButon() {
-        SceneManager.LoadScene("mainSettings");
+        // profile button
     }
 
     public void touchAboutButton() {
         SceneManager.LoadScene("mainAbout");
-    }
-
-    public void touchLoginWithFacebookButton() {
-        // do facebook login here...
-
-        loginAlert.SetActive(false);
-    }
-
-    public void touchCloseAlertButton() {
-        // do not login, stay at main menu
-
-        loginAlert.SetActive(false);
     }
 }
