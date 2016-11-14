ce07af2ddce2ca6eb8a33e3b035408ab8a80a2be
David Spehar <spehar@umich.edu>
2016-11-13 21:06:38 -0500

    leaderboards update

---
+++ Assets/leaderboards.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class leaderboards : MonoBehaviour {
+
+    public Image leaderboardImage;
+    public Text leaderBoardText;
+
+    public Sprite globalSprite;
+    public Sprite friendSprite;
+
+
+	// Use this for initialization
+	void Start () {
+	    if (PlayerPrefs.HasKey("leaderboard_mode"))
+        {
+            string mode = PlayerPrefs.GetString("leaderboard_mode");
+            if (mode == "global")
+            {
+                leaderboardImage.sprite = globalSprite;
+                leaderBoardText.text = "Global Leaderboards";
+            } else
+            {
+                leaderboardImage.sprite = friendSprite;
+                leaderBoardText.text = "Friend Leaderboards";
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/setButtonLeaderboardMode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setButtonLeaderboardMode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setGlobal() {
+        PlayerPrefs.SetString("leaderboard_mode", "global");
+    }
+
+    public void setFriend() {
+        PlayerPrefs.SetString("leaderboard_mode", "friend");
+    }
+}
